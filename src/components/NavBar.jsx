f34e5556c3964488f2e4f97e3aaa3de34b07f281
Niklas Mejlvang <niklasjbm@gmail.com>
2025-10-20 21:10:19 +0200

    made initial navbar with minimal styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import './NavBar.css';
 
 const NavBar = () => {
   return (
-    <nav>
-      <NavLink to="/" className={({ isActive }) => (isActive ? "text-blue-500" : "")}>
-        Home
-      </NavLink>
-
-      <p>Navbar</p>
-    </nav>
+    <div className="nav-all">
+      <nav className="nav-left">
+        <NavLink to="/" style={{ textDecoration: 'none' }} className={({ isActive }) => (isActive ? "text-blue-500" : "")}>
+          Ember
+        </NavLink>
+      </nav>
+      <nav className="nav-middle">
+        <NavLink to="/" style={{ textDecoration: 'none' }} className={({ isActive }) => (isActive ? "text-blue-500" : "")}>
+          About Us
+        </NavLink>
+        <NavLink to="/" style={{ textDecoration: 'none' }} className={({ isActive }) => (isActive ? "text-blue-500" : "")}>
+          Events
+        </NavLink>
+      </nav>
+      <nav className="nav-right">
+        <NavLink to="/" style={{ textDecoration: 'none' }} className={({ isActive }) => (isActive ? "text-blue-500" : "")}>
+          Sign in
+        </NavLink>
+      </nav>
+    </div>
   );
 };
 
