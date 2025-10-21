fa9986bb06c8c8ae0adf606686b12e9fe9ad2d18
Niklas Mejlvang <niklasjbm@gmail.com>
2025-10-21 13:38:30 +0200

    minor improvements

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -4,14 +4,19 @@ export default function Event(props) {
     return (
         <button className='event'
             style={{ backgroundImage: `url(${props.picture})` }}
-
-
-        >hello
-            {props.category}
-            {props.title}
-            {props.host}
-            {props.data}
-            {props.attendents}
-            {props.saved}    </button>
+        >
+            <top>
+                {props.category}
+                {props.saved ? ":)" : ":("}
+            </top>
+            <title>
+                {props.title}
+            </title>
+            <footer>
+                {props.host}
+                {props.date}
+                {props.attendents}
+            </footer>
+        </button>
     )
 }
