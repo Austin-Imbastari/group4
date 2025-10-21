6758c90f94272354678172f550622ce53656aa6b
Asger P <aspj@itu.dk>
2025-10-21 11:34:14 +0200

    Add SignOut and SignUp components

---
+++ src/pages/auth/SignOut.jsx
@@ -0,0 +1,9 @@
+export default function SignOut() {
+  return (
+    <>
+      <h2>Hi *name*!</h2>
+      <text>Do you want to sign out?</text>
+      <button type="submit">Sign Out</button>
+    </>
+  );
+}
