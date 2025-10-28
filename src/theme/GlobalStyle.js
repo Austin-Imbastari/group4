d454f2d56737c46af5a1ed348ba781277cc027cb
Asger P <aspj@itu.dk>
2025-10-28 13:51:43 +0100

    remove unused globalstyles

--- src/theme/GlobalStyle.js
+++ src/theme/GlobalStyle.js
@@ -15,37 +15,4 @@ export const GlobalStyle = createGlobalStyle`
         scroll-behavior: smooth;
     }
 
-    label {
-    margin-bottom: 0.5rem;
-    line-height: 1.25;
-    font-weight: 500;
-    color: #000;
-  }
-
-  textarea {
-    min-height: 10rem;
-    resize: vertical;
-    padding-left: 2.5rem;
-    padding-top: 1rem;
-  }
-
-  input,
-  textarea {
-    padding: 0.75rem 1rem;
-    line-height: 1.4;
-    border: 1px solid ${({ theme }) => theme.colors.accentDark};
-    border-radius: 12px;
-    background-color: #f3f3f5;
-    appearance: none;
-    -webkit-appearance: none;
-    box-shadow: none;
-    width: 100%;
-    box-sizing: border-box;
-    padding-left: 2.5rem;
-    &:focus {
-      outline: none;
-      box-shadow: none;
-    }
-  }
-
 `;
