07b7a3db8f6f68d0f790fd0944d3ef3a6a6a778e
Asger P <aspj@itu.dk>
2025-11-11 15:35:59 +0100

    Implement sign-in functionality with form handling and authentication context

--- src/pages/auth/SignIn.jsx
+++ src/pages/auth/SignIn.jsx
@@ -1,18 +1,40 @@
 import Button from "../../components/button/Button";
 import InputField from "../../components/input_field/InputField";
 import { User, Lock } from "lucide-react";
+import { useAuth } from "../../hooks/useAuth";
+import { useState } from "react";
 
 export default function SignIn() {
+  const { signIn, setAuthStep } = useAuth();
+  const [form, setForm] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = ({ target: { name, value } }) =>
+    setForm((prev) => ({ ...prev, [name]: value }));
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await signIn({
+      username: form.username,
+      password: form.password,
+    });
+    setAuthStep("signedin");
+  };
+
   return (
     <>
       <AuthHeader>
         <h2 className="title">WELCOME BACK</h2>
         <p className="description">Please enter your credentials</p>
       </AuthHeader>
-      <form>
+      <form onSubmit={handleSubmit}>
         <InputField
           label="Username"
           name="username"
+          value={form.username}
+          onChange={handleChange}
           placeholder="Enter your username"
           icon={User}
         />
@@ -20,6 +42,8 @@ export default function SignIn() {
           label="Password"
           name="password"
           type="password"
+          value={form.password}
+          onChange={handleChange}
           placeholder="Enter your password"
           icon={Lock}
         />
