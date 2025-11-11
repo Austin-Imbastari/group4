8219227872de437c3b6f12c3608369e102a89598
Asger P <aspj@itu.dk>
2025-11-11 15:42:30 +0100

    Make AuthPage render component based on context

--- src/pages/auth/AuthPage.jsx
+++ src/pages/auth/AuthPage.jsx
@@ -1,12 +1,19 @@
 import { AuthContainer, AuthPageWrapper } from "./AuthPageSC";
 import SignUp from "./SignUp";
+import SignIn from "./SignIn";
+import SignOut from "./SignOut";
+import { useAuth } from "../../hooks/useAuth";
 
 export default function AuthPage() {
+  const { authStep } = useAuth();
+
   return (
     <AuthPageWrapper>
       <AuthContainer>
         {/* TODO: Render SignIn, SignOut, or SignUp based on auth state */}
-        <SignUp />
+        {authStep === "signin" && <SignIn />}
+        {authStep === "signout" && <SignOut />}
+        {authStep === "signup" && <SignUp />}
       </AuthContainer>
     </AuthPageWrapper>
   );
