5132873f65e6311727ce0346afbaf9b19628878b
Asger P <aspj@itu.dk>
2025-11-11 13:42:51 +0100

    Add getUserCount cloud function

--- src/lib/parseService.js
+++ src/lib/parseService.js
@@ -1,5 +1,6 @@
 import { getParse } from "./parseClient.js";
 
+// Authentication - Create User
 export async function createUser({ username, email, password }) {
   const Parse = getParse();
   const user = new Parse.User();
@@ -7,4 +8,10 @@ export async function createUser({ username, email, password }) {
   user.set("email", email);
   user.set("password", password);
   return user.signUp();
+}
+
+// Cloud Function - Get User Count
+export async function getUserCount() {
+  const Parse = getParse();
+  return Parse.Cloud.run("countUsers");
 }
