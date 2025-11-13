0cef64f3bd1e12920db6ea1d72e361a42f478382
Niklas Mejlvang <niklasjbm@gmail.com>
2025-11-13 15:51:51 +0100

    fixed navbar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -26,19 +26,22 @@ const NavBar = () => {
         </NavLink>
       </NavBarCenterContainer>
       {user?.username ? (
-        <div style={{ marginTop: 12 }}>
-          <span>
-            <strong>{user.username}</strong>
-          </span>
-        </div>
+        <NavLink to="/auth">
+          <div style={{ marginTop: 12 }}>
+            <span>
+              <strong >{user.username}</strong>
+            </span>
+          </div>
+        </NavLink>
       ) : (
         <NavLink to="/auth">
           <NavBarSignIn>
             <span>Sign in</span>
           </NavBarSignIn>
         </NavLink>
-      )}
-    </NavBarContainer>
+      )
+      }
+    </NavBarContainer >
   );
 };
 
