005a8cfa72695e951c915167961cd7d6b9ca3042
Asger P <aspj@itu.dk>
2025-11-13 07:59:51 +0100

    Set AuthContainer width to be consistent across forms

--- src/pages/auth/AuthPageSC.js
+++ src/pages/auth/AuthPageSC.js
@@ -8,20 +8,8 @@ export const AuthPageWrapper = styled.div`
   padding: 1rem;
 `;
 
-export const AuthHeader = styled.div`
-  text-align: center;
-
-  .title {
-    margin-bottom: 0.5rem;
-    font-size: ${(props) => props.theme.fontSizes.large};
-  }
-
-  .description {
-    font-size: ${(props) => props.theme.fontSizes.small};
-  }
-`;
-
 export const AuthContainer = styled.div`
+  width: 100%;
   max-width: 400px;
   background-color: white;
   border: 1px solid ${(props) => props.theme.colors.primary};
@@ -35,8 +23,22 @@ export const AuthContainer = styled.div`
   form {
     width: 100%;
     display: flex;
+    align-items: stretch;
     flex-direction: column;
     gap: 1rem;
+  }
+`;
+
+export const AuthHeader = styled.div`
+  text-align: center;
+
+  .title {
+    margin-bottom: 0.5rem;
+    font-size: ${(props) => props.theme.fontSizes.large};
+  }
+
+  .description {
+    font-size: ${(props) => props.theme.fontSizes.small};
   }
 `;
 
