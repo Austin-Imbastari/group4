b0a78a087ed8fbbb1336033570ac0407a7d3a25f
Niklas Mejlvang <niklasjbm@gmail.com>
2025-11-26 10:36:17 +0100

    Fixed Mircea's requests for 'All events'.

---
+++ src/pages/all_events/Event.jsx
@@ -0,0 +1,31 @@
+import { NavLink } from "react-router-dom";
+import {
+    EventContainer,
+    EventTopContainer,
+    EventCenterContainer,
+    EventBottomContainer,
+} from "./AllEventSC";
+
+export default function EventCard({ event }) {
+    return (
+        <NavLink to={`/events/${event.id}`} style={{ textDecoration: "none" }}>
+            <EventContainer>
+                <EventTopContainer $bg={event.picture}>
+                    <span>{event.category}</span>
+                    <span>{event.saved ? ":)" : ":("}</span>
+                </EventTopContainer>
+
+                <EventCenterContainer>
+                    <span>{event.title}</span>
+                </EventCenterContainer>
+
+                <EventBottomContainer>
+                    <span>[ . ] By: {event.host}</span>
+                    <span className="span2">
+                        {event.date} {event.time ? `@ ${event.time}` : ""} * {event.attendents} attendees
+                    </span>
+                </EventBottomContainer>
+            </EventContainer>
+        </NavLink>
+    );
+}
