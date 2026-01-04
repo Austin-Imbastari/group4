98df0aab31a4fef539381edbdcb953f8d4c0219a
Asger P <aspj@itu.dk>
2026-01-04 14:26:18 +0100

    Refactor button components to remove unused props and streamline code

--- src/components/button/BackButton.jsx
+++ src/components/button/BackButton.jsx
@@ -1,9 +1,5 @@
-import {BackButtonSC } from "./ButtonSC";
+import { BackButtonSC } from "./ButtonSC";
 
-export default function BackButton({ children, onClick, type = "button"}) {
-    return(
-    <BackButtonSC type={type} onClick={onClick}>
-      {children}
-    </BackButtonSC>
-    )
+export default function BackButton({ children, onClick }) {
+  return <BackButtonSC onClick={onClick}>{children}</BackButtonSC>;
 }
--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,8 +1,5 @@
 import { ButtonSC } from "./ButtonSC";
 
-export default function Button({ children, onClick, type = "button"}) {
-  return (
-      <ButtonSC type={type} onClick={onClick}>
-      {children}
-    </ButtonSC>)
+export default function Button({ children, onClick }) {
+  return <ButtonSC onClick={onClick}>{children}</ButtonSC>;
 }
